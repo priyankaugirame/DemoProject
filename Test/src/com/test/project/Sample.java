1545e7ea09a64cef0ecf4f2cef0c8ddacf1f5598
Priyanka <zurangepriyanka433@gmail.com>
2024-06-30 12:28:14 +0530

    first commit

--- Test/src/com/test/project/Sample.java
+++ Test/src/com/test/project/Sample.java
@@ -1,11 +1,9 @@
 package com.test.project;
 
-import java.security.DomainCombiner;
-
 public class Sample {
-		public static void main(String[] args) {
-			for(int i=1;i<=10;i++)
-				System.out.println(i);
-		}
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++)
+			System.out.println(i);
+	}
 
 }
